e25f4cccd81e8d191280601d43c526d4538c1169
ProgramWarfare <95079489+ProgramWarfare@users.noreply.github.com>
2022-03-27 18:31:31 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,25 @@
+"use strict";
+var btnContainer = document.getElementById("kvar");
+
+
+var btns = btnContainer.getElementsByClassName("meny");
+
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+function myFunction() {
+    var x = document.getElementById("kvar");
+    if (x.className === "meny") {
+      x.className += " responsive";
+    } else {
+      x.className = "meny";
+    }
+  }
+
+
---
+++ js/tidszon.js
@@ -0,0 +1,14 @@
+"use strict";
+var btnContainer = document.getElementById("kvar");
+
+
+var btns = btnContainer.getElementsByClassName("li");
+
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
