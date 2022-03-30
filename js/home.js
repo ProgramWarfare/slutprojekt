b00501a787db7045e8e6b3e1d5251c1dbc0ad8da
ProgramWarfare <95079489+ProgramWarfare@users.noreply.github.com>
2022-03-30 14:26:32 +0200

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,18 +1,4 @@
 "use strict";
-var btnContainer = document.getElementById("kvar");
-
-
-var btns = btnContainer.getElementsByClassName("meny");
-
-
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-
 function myFunction() {
     var x = document.getElementById("kvar");
     if (x.className === "li") {
